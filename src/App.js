ae81381da852b12e5ef0db415774ee525ba436ea
Colin <colin.diesh@gmail.com>
2019-11-28 14:32:00 -0500

    Find actual pain point at 200000 nodes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@ import React, {useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-const RandomThing = () => {
-  return (<div>
-    {Array.from({length: 5000}, (v, i) => i).map(val =>
-      <div key={val}>{val}</div>
-    )}
-  </div>)
-}
-
-function App() {
+const RotatingThingy = () => {
   const [angle, setAngle] = useState(0)
   useEffect(() => {
     const ret = window.requestAnimationFrame(() => {
@@ -20,21 +11,24 @@ function App() {
     return () => window.cancelAnimationFrame(ret)
   })
   return (
+    <img style={{transform: `rotate(${angle}deg)`}} src={logo} className="App-logo" alt="logo" />
+  )
+}
+
+const RandomThing = () => {
+  console.log('here')
+  return (<div>
+    {Array.from({length: 200000}, (v, i) => i).map(val =>
+      <div key={val}>{val}</div>
+    )}
+  </div>)
+}
+
+function App() {
+
+  return (
     <div className="App">
-      <header className="App-header">
-        <img style={{transform: `rotate(${angle}deg)`}} src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RotatingThingy />
       <RandomThing />
     </div>
   );
